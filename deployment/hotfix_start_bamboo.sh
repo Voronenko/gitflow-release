87601d3d9c79af54725f21b5a77f5deed1dde65a
Vyacheslav Voronenko <git@voronenko.info>
2016-03-11 17:07:51 +0200

    Improve(deployment) support for hotfixing automation

---
+++ deployment/hotfix_start_bamboo.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+cd ${PWD}/../
+
+VERSION=$1
+if [ -z $1 ]
+then
+  echo "Please provide uniqie hotfix name. Jira ticket number is a good candidate"
+  exit 1
+fi
+
+# PREVENT INTERACTIVE MERGE MESSAGE PROMPT
+GIT_MERGE_AUTOEDIT=no
+export GIT_MERGE_AUTOEDIT
+GIT_REMOTE=git@github.com:Voronenko/bamboo-release.git
+
+
+# add remote due to bamboo git cache shit
+git remote add central "$GIT_REMOTE"
+
+#Initialize gitflow
+git flow init -f -d
+
+# ensure you are on latest develop  & master
+git checkout develop
+git pull central develop
+git checkout -
+
+git checkout master
+git pull central master
+git checkout develop
+
+git flow hotfix start $VERSION
+
+# bump hotfixed version to server
+git push central hotfix/$VERSION
